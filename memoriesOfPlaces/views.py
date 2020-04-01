71182ba542af8cc9536f9ea49a9b713c28114976
ncw <whitesky999@naver.com>
2020-04-01 21:24:03 +0900

    django template test for bring DB data

--- memoriesOfPlaces/views.py
+++ memoriesOfPlaces/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Memories
 
 # Create your views here.
 
 def memories_list(request):
-    return render(request, 'index.html', {})
+    memories = Memories.objects.filter(mem_date__lte=timezone.now()).order_by('mem_date')
+    return render(request, 'index.html', {'memories' : memories})
